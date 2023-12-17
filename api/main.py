5600b31af58dce3bbb82ae491d96e51d01ea5f60
Jose A. Pinell <japinell@yahoo.com>
2023-12-17 12:47:07 -0600

    Single image endpoint with delete method enabled

--- api/main.py
+++ api/main.py
@@ -57,5 +57,17 @@ def images():
         raise EnvironmentError("Invalid operation")
 
 
+@app.route("/images/<image_id>", methods=["DELETE"])
+def image(image_id):
+    if request.method == "DELETE":
+        # Delete image from the database
+        result = images_collection.delete_one({"_id": image_id})
+        if not result:
+            return {"error": "Image was not deleted"}, 500
+        if result and not result.deleted_count:
+            return {"error": "Image not found"}, 404
+        return {"deleted_id": image_id}
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
